14dfe904cf73fc0f382c94977e950eec0bd6ff24
LucasMo007 <easoncoldplay2@gmail.com>
2025-10-16 00:32:45 -0400

    Set name and make some draw
    Draw ground ,platform and circle

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -3,13 +3,40 @@
 
 int main()
 {
-    InitWindow(800, 800, "Physics-1");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+    InitWindow(800, 800, "Angry Bird: Rendering Launch Vector");
+    SetTargetFPS(60);
 
+    Rectangle platform;
+    platform.x = 0.0f;
+    platform.y = 600.0f;
+    platform.width = 100.0f;
+    platform.height = 20.0f;
+
+    Rectangle ground;
+    ground.x = 0.0f;
+    ground.y = 780.0f;
+    ground.width = 800.0f;
+    ground.height = 20.0f;
+
+    float radius = 10.0f;
+    Vector2 launchPosition;
+    launchPosition.x = platform.x + platform.width - radius;
+    launchPosition.y = platform.y - radius;
     while (!WindowShouldClose())
     {
         BeginDrawing();
-            ClearBackground(WHITE);
-            DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
+        ClearBackground(RAYWHITE);
+
+
+        DrawRectangleRec(platform, GRAY);
+
+
+        DrawRectangleRec(ground, DARKGRAY);
+
+
+        DrawCircleV(launchPosition, radius, RED);
+
         EndDrawing();
     }
 
