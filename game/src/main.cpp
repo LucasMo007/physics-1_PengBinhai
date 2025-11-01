5163b9a246a1c72e7272140f25fd4c6ca3459f0e
LucasMo007 <easoncoldplay2@gmail.com>
2025-11-01 17:50:57 -0400

    Initiate the third Lab project
    create a  window
    define a platform with set size
    define a ground
    draw a platform
    draw a ground

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -345,4 +345,45 @@
 //        CloseWindow();
 //        return 0;
 //    }
+#include "raylib.h"
+#include "raymath.h"
+
+
+int main() {
+
+    InitWindow(800, 800, "Angry Bird: initial velocity vector (raylib)");
+
+    //define a platform  with set size
+    Rectangle platform;
+    platform.x = 0.0f;
+    platform.y = 600.0f;
+    platform.width = 100.0f;
+    platform.height = 20.0f;
+
+    //create a ground rectangle across the bottom of the screen
+    Rectangle ground;
+    ground.x = 0.0f;
+    ground.y = 780.0f;
+    ground.width = 800.0f;
+    ground.height = 20.0f;
+
+
+    while (!WindowShouldClose()) {
+
+        float t = GetTime(); // get total time since the program started
+        float dt = GetFrameTime();// get time passed since last frame
+
+        BeginDrawing();
+        ClearBackground(WHITE);//white background 
+
+        DrawRectangleRec(platform, GRAY);//draw platform 
+
+        DrawRectangleRec(ground, DARKGRAY);//draw ground 
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
 
